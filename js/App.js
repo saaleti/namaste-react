13f0bed13b59006aea05bf3b6dd37df5a82fcb34
SwaroopAaleti <swaroop.aaleti@mckesson.com>
2025-06-16 15:26:55 +0530

    Uploading Episode 3 code

--- js/App.js
+++ js/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client"
 
@@ -10,11 +11,24 @@ import ReactDOM from "react-dom/client"
  * 
  */
 
+// Creating JSX using function
 
-const divParent = React.createElement("div",{"id":"parent"},
-    React.createElement("div",{"id":"child"},
-        [React.createElement("h1",{}, "Im h1 tag"),React.createElement("h2",{}, "Im h2 tag")]));
+const FunctionalComponentTitle = () => <h1> This is React Functional Component Title</h1>;
+
+
+const name ="Swaroop";
+
+const FunctionalComponentHeading = () =>  { return ( 
+<div>
+{FunctionalComponentTitle()}
+<FunctionalComponentTitle/>
+<FunctionalComponentTitle></FunctionalComponentTitle>
+<h2> This is React Functional Component Heading</h2>
+<h3> Hello {name}</h3>
+</div>
+)};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(divParent);
+root.render(<FunctionalComponentHeading/>);
